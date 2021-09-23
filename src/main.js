75fdd8844ceb8ab57470af79f9a5cf9b5e73512d
Can Altıparmak <can6parmak@gmail.com>
2021-09-23 19:42:28 +0300

    Fix bug introduced on a4ef3e7

--- src/main.js
+++ src/main.js
@@ -21,12 +21,15 @@ function main() {
   logseq.provideStyle(`
     div[data-injected-ui=show-habits-${key}] {
       font-size: 20px;
+      padding: 0.25rem;
+      margin: 0 0.25rem;
+      border-radius: 3px;
     }
   `)
 
   logseq.App.registerUIItem('toolbar', {
     key: 'show-habits',
-    template: `<a class="button" data-on-click="show" title="Habits" class="opacity-60 hover:opacity-100">🗹</a>`,
+    template: `<a data-on-click="show" title="Habits" class="opacity-60 hover:opacity-100">🗹</a>`,
   })
 
   createApp(App).mount('#app');
